9d613f062589ec38880e815340e52e1de4f27662
rozklad <jan.rozklad@gmail.com>
2016-04-15 09:26:06 +0200

    profile dependencies and docs

---
+++ README.md
@@ -0,0 +1,15 @@
+# Profile
+
+Extended user profile for Cartalyst Platform.
+
+## Documentation
+
+## Requirements
+
+- Package requires PHP 5.6.0 (using ARRAY_FILTER_USE_BOTH)
+
+## Changelog
+
+## Support
+
+## License
--- src/Repositories/AuthRepository.php
+++ src/Repositories/AuthRepository.php
@@ -14,7 +14,7 @@ class AuthRepository extends BaseAuthRepository implements AuthRepositoryInterfa
     public function getSocialConnections()
     {
         return array_filter($this->container['sentinel.addons.social']->getConnections(), function ($connection, $key) {
-            return ($connection['identifier'] && $connection['secret'] && config('sanatorium-social.connections.'.$key));
+            return ($connection['identifier'] && $connection['secret'] && config('sanphp atorium-social.connections.'.$key));
         }, ARRAY_FILTER_USE_BOTH);
     }
 }
